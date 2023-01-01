d96938aed03ca9e77be78dfdea0cd48ffbe07f17
ashwinam <ashwinam31@gmail.com>
2023-01-01 20:03:00 +0530

    JS chapter5 practice set 2

--- ch05_ps.js
+++ ch05_ps.js
@@ -17,3 +17,27 @@ while(true){
 }
 
 console.log(numArr);
+
+//3. filter for numbers divisible by 10 from a given array
+
+let arr = [10, 20, 40, 55, 33, 60, 90]
+
+console.log(arr.filter((val) =>{
+    return val % 10 === 0
+})); // [10, 20, 40, 60, 90]
+
+// Q4. create an array of square of a number
+
+console.log(arr.map((val) => {
+    return val * val
+})); // [100, 400, 1600, 3025, 1089, 3600, 8100]
+
+// Q5. use reduce to calculate factorial of a given number from an array of first n natural numbers (n being the number whose factorial needs to be calculated.)
+
+arr = [1, 2, 3, 4, 5, 6]
+
+let factorial = (val1, val2) =>{
+    return val1 * val2
+}
+
+console.log(arr.reduce(factorial));
