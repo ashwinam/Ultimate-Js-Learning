09b11e8c719a87ba2ec43daa2ae0939da6be86d2
ashwinam <ashwinam31@gmail.com>
2023-01-08 12:18:37 +0530

    JS chapter 06 practice set harry bhai solution added

--- ch06_ps.js
+++ ch06_ps.js
@@ -86,3 +86,37 @@ if (userChoice){
 }else{
     alert('Give the proper input.');
 }
+
+// Harry bhai solution
+let runAgain = true;
+
+const canDrive = (age) => {
+  return age >= 18 ? true : false
+}
+
+while (runAgain) {
+  let age = prompt("Enter your age")
+  age = Number.parseInt(age)
+  if (age < 0) {
+    console.error("Please enter a valid age");
+    break;
+  }
+
+  if (canDrive(age)) {
+    alert("Yes you can drive")
+  }
+  else {
+    alert("You cannot drive")
+  }
+  runAgain = confirm("Do you want to play again?")
+}
+
+// let number = prompt("Enter your number")
+// number = Number.parseInt(number)
+
+// if (number > 4) {
+//   location.href = "https://google.com"
+// }
+
+let color = prompt("Enter the page background color")
+document.body.style.background = color
