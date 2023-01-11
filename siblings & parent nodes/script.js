c69aa1dfeaae806ec8b655d782786b10ed1de032
ashwinam <ashwinam31@gmail.com>
2023-01-11 20:29:42 +0530

    JS siblings & parents update

--- siblings & parent nodes/script.js
+++ siblings & parent nodes/script.js
@@ -1,4 +1,5 @@
 console.log(document.body.firstChild.nextSibling);
 console.log(document.body.firstChild.previousSibling);
 console.log(document.body.firstChild.parentElement);
-console.log(document.body.firstChild.parentNode);
+// if element not present then it will be give a null but with Node it always return something.
+console.log(document.body.firstChild.parentNode); 
