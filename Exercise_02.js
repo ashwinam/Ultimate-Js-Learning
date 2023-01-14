bc5fb38ada5c013152788da8ffce6b16a9aa0503
ashwinam <ashwinam31@gmail.com>
2023-01-14 20:03:36 +0530

    JS Exercise 02 harry bhai solution

--- Exercise_02.js
+++ Exercise_02.js
@@ -58,3 +58,35 @@ if (uInput === 's' && compInput === 'w'){
     alert('Draw...')
 }
 }while(uInput === compInput);
+
+
+// Harry Bhai Solution
+let user = prompt("Enter S, W or G")
+let cpuI = Math.floor(Math.random() * 3);
+let cpu = ["S", "W", "G"][cpuI]
+
+const match = (cpu, user)=>{
+  if(cpu === user){
+    return "Nobody"
+  }
+  else if(cpu === "S" && user==="W"){
+    return "cpu"
+  }
+  else if(cpu === "S" && user==="G"){
+    return "user"
+  }
+  else if(cpu === "G" && user==="W"){
+    return "user"
+  }
+  else if(cpu === "G" && user==="S"){
+    return "cpu"
+  }
+  else if(cpu === "W" && user==="S"){
+    return "user"
+  }
+  else if(cpu === "W" && user==="G"){
+    return "cpu"
+  }
+}
+let result = match(cpu, user)
+document.write(`CPU:${cpu} <br> User:${user} <br>The winner is: ${result.toUpperCase()}` )
